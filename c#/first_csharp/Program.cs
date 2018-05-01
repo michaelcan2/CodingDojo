dc3efac0f9aead79633ced18e324265534b8e26f
Michael cantu <michaelcan293@gmail.com>
2018-04-30 19:32:55 -0500

    no msg

---
+++ c#/first_csharp/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+namespace first_csharp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Console.WriteLine("It worked!!!");
+
+           int favoriteNum=9;
+           Console.WriteLine("You can Print numbers...");
+           Console.WriteLine(favoriteNum);
+           Console.WriteLine("the {0} cow, jumped over the {1}, {2} times!", "brown", "Moon", 8);
+
+       
+for (int i =1; i<255; i++)
+{
+    System.Console.WriteLine(i);
+}
+
+
+for(int j=1; j<100; j++)
+{
+    if(j%3==0 && j%5==0)
+    {
+        continue;
+    } else if(j%5==0 || j%3==0)
+    {
+        System.Console.WriteLine(j);
+    }
+}
+
+
+
+
+
+
+
+
+        }
+    }
+}
