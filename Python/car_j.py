b829a3c1fb6965c6d5f7d3bdd1b2fecb55e4fdad
Michael cantu <michaelcan293@gmail.com>
2018-08-02 11:57:54 -0500

    nomsg

---
+++ Python/car_j.py
@@ -0,0 +1,17 @@
+class Car:
+    def __init__(self,price,speed,fuel,mileage)
+        self.price = price
+        self.speed = speed
+        self.fuel = fuel
+        self.mileage = mileage
+
+    def display_all(self)
+    print(self.price,self.speed,self.fuel,self.mileage)
+
+
+car1 = Car(300,30,2,10)
+
+
+car1.display_all()
+
+
